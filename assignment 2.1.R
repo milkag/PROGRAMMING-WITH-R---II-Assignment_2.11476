08fc60aee6dee5f5338a973fa264db6fbcec6ba9
milkag <35522436+milkag@users.noreply.github.com>
2018-12-24 23:28:58 +0200

    Add files via upload

---
+++ assignment 2.1.R
@@ -0,0 +1,22 @@
+
+myMatrix = matrix(data = range(0),nrow = 5,ncol = 5,byrow = TRUE)
+myMatrix
+
+ctr = 0
+for (i in 1:nrow(myMatrix)){
+  for (j in 1:nrow(myMatrix)){
+    if (i == j){
+      myMatrix[i, j] = 1
+      break
+    }
+    else {
+      myMatrix[i, j] = i + j
+      ctr = ctr + 1
+    }
+    
+  }
+  
+}
+
+print(myMatrix)
+print(ctr)
